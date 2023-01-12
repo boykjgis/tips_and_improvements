cd9646e3cc55783d183882ae055613bec1b189ba
boykjgis <boykjgis@gmail.com>
2023-01-12 16:50:55 -0700

    unpacking.py: Add additional improved code line

--- unpacking.py
+++ unpacking.py
@@ -5,3 +5,22 @@ a, b = (1, 2)
 
 print(a)
 print(b)
+
+# Refactored code
+a, _ = (1, 2)
+
+print(a)
+#print(b)
+
+# a, b = (1, 2, 3, 4)
+# Will result in a value error -> Too many values to unpack (ValueError)
+
+# a, b, c = (1, 2)
+# Will result in not enough values to unpack (ValueError)
+
+a, b, *c = (1, 2, 3, 4, 5)
+print(a, b, c)
+
+a, b, *_ = (1, 2, 3, 4, 5)
+print(a, b)
+print(_)
